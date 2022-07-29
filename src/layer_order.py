076f5709bce8d7eb1a227c26000d55b02a1bac6c
geoxyIT <geoxy.box@gmail.com>
2022-07-29 15:17:38 +0200

    Stworzenie config z warstwami i stylizowanie dla warstw zg z rozpo

--- src/layer_order.py
+++ src/layer_order.py
@@ -1,6 +1,7 @@
 from qgis.core import *
 from qgis.utils import iface
 from typing import List
+from .config import correct_layers
 
 def set_new_order(layer_new_order: List):
     # wejscie do roota
@@ -45,58 +46,6 @@ def set_new_order(layer_new_order: List):
     return set_custom
 
 # wywolanie funkcji
-order_list_new = ['EGB_etykieta',
-                          'OT_etykieta',
-                          'GES_etykieta',
-                          'EGB_JednostkaEwidencyjna',
-                          'EGB_ObrebEwidencyjny',
-                          'EGB_DzialkaEwidencyjna',
-                          'EGB_PunktGraniczny',
-                          'EGB_Budynek',
-                          'OT_BudynekNiewykazanyWEGIB',
-                          'EGB_BlokBudynku',
-                          'OT_BlokBudynku',
-                          'EGB_ObiektTrwaleZwiazanyZBudynkiem',
-                          'EGB_poliliniaKierunkowa',
-                          'OT_ObiektTrwaleZwiazanyZBudynkami',
-                          'OT_poliliniaKierunkowa',
-                          'OT_Budowle',
-                          'OT_Ogrodzenia',
-                          'OT_Komunikacja',
-                          'GES_PrzewodWodociagowy',
-                          'GES_UrzadzeniaSiecWodociagowa',
-                          'GES_PrzewodKanalizacyjny',
-                          'GES_UrzadzeniaSiecKanalizacyjna',
-                          'GES_PrzewodElektroenergetyczny',
-                          'GES_UrzadzeniaSiecElektroenergetyczna',
-                          'GES_PrzewodGazowy',
-                          'GES_UrzadzeniaSiecGazowa',
-                          'GES_PrzewodCieplowniczy',
-                          'GES_UrzadzeniaSiecCieplownicza',
-                          'GES_PrzewodTelekomunikacyjny',
-                          'GES_UrzadzeniaSiecTelekomunikacyjna',
-                          'GES_PrzewodSpecjalny',
-                          'GES_UrzadzeniaTechniczneSieciSpecjalnej',
-                          'GES_PrzewodNiezidentyfikowany',
-                          'GES_UrzadzenieNiezidentyfikowane',
-                          'GES_UrzadzeniaTowarzyszczaceLiniowe',
-                          'GES_UrzadzeniaTowarzyszaceLiniowe',
-                          'GES_InneUrzadzeniaTowarzyszace',
-                          'OT_SportIRekreacja',
-                          'OT_ZagospodarowanieTerenu',
-                          'EGB_KonturUzytkuGruntowego',
-                          'EGB_KonturKlasyfikacyjny',
-                          'OT_Wody',
-                          'OT_Rzedna',
-                          'GES_Rzedna',
-                          'OT_Skarpa',
-                          'OT_poczatekGorySkarpy',
-                          'OT_koniecGorySkarpy',
-                          'EGB_PrezentacjaGraficzna',
-                          'EGB_odnosnik',
-                          'OT_PrezentacjaGraficzna',
-                          'OT_odnosnik',
-                          'GES_PrezentacjaGraficzna',
-                          'GES_odnosnik']
+order_list_new = correct_layers
     
 set_new_order(order_list_new)
