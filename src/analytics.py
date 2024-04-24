01105cdf5245d6488c1040c5c2b03391712f20e8
Kaemer1645 <szymex23@o2.pl>
2024-04-24 14:29:45 +0200

    Dodanie funkcji do zbierania statystyk

--- src/analytics.py
+++ src/analytics.py
@@ -1,9 +1,15 @@
+import ctypes
+from QMapa import STAT_DLL
 
-def runAnalytics(analysis_code, analysis_info = ''):
+def runAnalytics(analysis_code, analysis_info = '') -> int:
     """analysis code: int
     analysis_info: string"""
     # otwarcie: 1
     # import: 2
     # redakcja: 3
     analysis_info = str(analysis_info)
-    #print(analysis_code, analysis_info)
+    lib = ctypes.CDLL(STAT_DLL)
+    lib.sendStatisticsExt.argtypes = [ctypes.c_int, ctypes.c_char_p]
+    lib.sendStatisticsExt.restype = ctypes.c_int
+    status = lib.sendStatisticsExt(analysis_code, analysis_info.encode('cp1250'))
+    return status
