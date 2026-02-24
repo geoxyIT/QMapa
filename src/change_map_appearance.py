7ab88d50f121d331044567eb8bc75c214200e1d2
wkopec00 <wojciechkopec@op.pl>
2026-02-24 15:58:00 +0100

    refractor: Przebudowanie funkji addOrtoOsm aby przyjmowala jako argument rozdzielczosc oraz WMS/WMTS

--- src/change_map_appearance.py
+++ src/change_map_appearance.py
@@ -6,6 +6,10 @@ from qgis.core import *
 from qgis.gui import *
 from .qmapa_main import Main
 from .express_yourself import ExpressYourself
+import urllib.parse
+
+from .config import geoportal_orto_url
+
 
 class ChangeAppearance():
     def __init__(self):
@@ -37,62 +41,82 @@ class ChangeAppearance():
             print('Błąd pobierania dodatkowych informacji:', e)
             print('Sprawdź swoje połączenie internetowe.')
 
-    def addOrtoOsm(self, service_type):
-        """Dodanie serwerów OSM i Geoportal ORTO jako warstwa do QGIS"""
-        name_orto = 'Geoportal ORTO'
-        name_osm = 'Open Street Map'
-        orto_url = 'https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution'
 
-        if service_type == 'GEOPORTAL_ORTO':
-            if len(QgsProject.instance().mapLayersByName(name_orto)) == 0:
-                urlWithParams_orto = 'contextualWMSLegend=0' \
-                                     '&crs=EPSG:2180' \
-                                     '&dpiMode=7' \
-                                     '&featureCount=10' \
-                                     '&format=image/jpeg' \
-                                     '&layers=Raster' \
-                                     '&styles' \
-                                     f'&url={orto_url}'
+    def addOrtoOsm(self, service_type, protocol='WMS'):
+        """
+        Dodanie serwerów OSM i Geoportal ORTO jako warstwa do QGIS.
+        
+        :param service_type: 'OSM', 'ORTO_STANDARD' lub 'ORTO_HIGH'
+        :param protocol: 'WMS' lub 'WMTS' (domyślnie 'WMS'. Ignorowane dla OSM)
+        """
+        layer_name = ""
+        uri = ""
+        
+        # generowanie URI
+        if service_type == 'OSM':
+            layer_name = 'Open Street Map'
+            uri = (
+                'type=xyz&'
+                'url=https://a.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&'
+                'zmax=19&zmin=0&crs=EPSG3857'
+            )
+            
+        elif service_type in ['ORTO_STANDARD', 'ORTO_HIGH']:
+            # Określenie rozdzielczości
+            is_high = (service_type == 'ORTO_HIGH')
+            res_path = "HighResolution" if is_high else "StandardResolution"
+            res_label = "Wysoka Rozdzielczość" if is_high else "Standardowa Rozdzielczość"
+            
+            # Baza URL Geoportalu
+            base_url = f"{geoportal_orto_url}{protocol}/{res_path}"
+            layer_name = f'Geoportal ORTO {res_label} ({protocol})'
+            
+            if protocol == 'WMS':
+                uri = (
+                    "IgnoreGetFeatureInfoUrl=1&IgnoreGetMapUrl=1&contextualWMSLegend=0&"
+                    "crs=EPSG:2180&format=image/jpeg&layers=Raster&styles=&"
+                    f"url={base_url}"
+                )
+            elif protocol == 'WMTS':
+                encoded_url = urllib.parse.quote(f"{base_url}?SERVICE=WMTS&REQUEST=GetCapabilities")
+                uri = (
+                    "contextualWMSLegend=0&crs=EPSG:2180&format=image/jpeg&"
+                    "layers=ORTOFOTOMAPA&styles=default&tileMatrixSet=EPSG:2180&"
+                    f"url={encoded_url}"
+                )
+            else:   # prtocol to nie WMS ani WMTS
+                return
+        else:   # service_type to nie ORTO_STANDARD ani ORTO_HIGH
+            return
 
-                rlayerOrto = QgsRasterLayer(urlWithParams_orto, name_orto, 'wms')
+        # sprawdzenie czy warstwa o takiej nazwie istnieje
+        if len(QgsProject.instance().mapLayersByName(layer_name)) > 0:
+            print(f'Warstwa z serwisem o nazwie {layer_name} już istnieje')
+            QMessageBox.critical(
+                iface.mainWindow(), 
+                'Dodanie serwisu nie powiodło się',
+                f'Warstwa "{layer_name}" już istnieje w projekcie.',
+                buttons=QMessageBox.StandardButton.Ok
+            )
+            return
 
-                if rlayerOrto.isValid():
-                    root = QgsProject.instance().layerTreeRoot()
-                    QgsProject.instance().addMapLayer(rlayerOrto, False)
-                    root.insertLayer(0, rlayerOrto)
-                    QgsProject.instance().layerTreeRoot().findLayer(rlayerOrto.id()).setItemVisibilityChecked(True)
-                else:
-                    print(f'Nieprawidłowa warstwa {name_orto}')
-            else:
-                print(f'Warstwa z serwisem o takiej nawie już istnieje')
-                QMessageBox.critical(iface.mainWindow(), 'Dodanie serwisu nie powiodło się',
-                                     'Warstwa z serwisem o takiej nazwie już istnieje.',
-                                     buttons=QMessageBox.StandardButton.Ok)
+        # utworzenie i dodanie warstwy do qgisa
+        layer = QgsRasterLayer(uri, layer_name, 'wms')
 
-
-        elif service_type == 'OSM':
-            if len(QgsProject.instance().mapLayersByName(name_osm)) == 0:
-                urlWithParams_osm = 'type=xyz' \
-                                    '&url=https://a.tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png' \
-                                    '&zmax=19' \
-                                    '&zmin=0' \
-                                    '&crs=EPSG3857'
-
-                rlayerOsm = QgsRasterLayer(urlWithParams_osm, name_osm, 'wms')
-
-                if rlayerOsm.isValid():
-                    root = QgsProject.instance().layerTreeRoot()
-                    QgsProject.instance().addMapLayer(rlayerOsm, False)
-                    root.insertLayer(0, rlayerOsm)
-                    QgsProject.instance().layerTreeRoot().findLayer(rlayerOsm.id()).setItemVisibilityChecked(True)
-                else:
-                    print(f'Nieprawidłowa warstwa {name_osm}')
-            else:
-                print(f'Warstwa z serwisem o takiej nawie już istnieje')
-                QMessageBox.critical(iface.mainWindow(), 'Dodanie serwisu nie powiodło się',
-                                     'Warstwa z serwisem o takiej nazwie już istnieje.',
-                                     buttons=QMessageBox.StandardButton.Ok)
-
+        if layer.isValid():
+            root = QgsProject.instance().layerTreeRoot()
+            QgsProject.instance().addMapLayer(layer, False)
+            root.insertLayer(0, layer)
+            root.findLayer(layer.id()).setItemVisibilityChecked(True)
+            print(f'Pomyślnie dodano warstwę: {layer_name}')
+        else:
+            print(f'Nieprawidłowa warstwa {layer_name}')
+            QMessageBox.critical(
+                iface.mainWindow(), 
+                'Błąd ładowania',
+                f'Nie udało się wczytać warstwy "{layer_name}"',
+                buttons=QMessageBox.StandardButton.Ok
+            )
 
     def getSelectedScale(self, scale_string):
         """rozpoznanie aktualnie wybranej skali, gdy nie rozpozna to wstawi domyslna skale 500"""
